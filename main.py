4351c4a6f2916dfc037fa1ba4557b278233b897b
Joshua <tkdkwok@gmail.com>
2018-11-11 10:17:58 -0500

    added more API calculations, geolocation still doesn't work

--- main.py
+++ main.py
@@ -28,3 +28,26 @@ user_posn['lat'] = user.geolocate(key)['location'][0]['lat']
 user_posn['lng'] = user.geolocate(key)['location'][0]['lng']
 
 print(user_posn['lat'])
+
+# gets the distance of all vehicles from the user
+vehicles_dist = []
+vehicles_dist = user.distance_matrix(user_posn, net_posns)
+
+count = 1
+shortest = int(vehicles_dist[0]['rows']['elements']['distance']['text'].split(' ')[0])
+
+
+closest = 0
+for v in range(len(vehicles_dist)):
+	if int(v[0]['rows']['elements']['distance']['text'].split(' ')[0]) < shortest:
+		closest = count
+		shortest = v
+		count = count + 1
+
+# get the directions from the selected vehicle to the user
+distance = shortest
+while(duration > 30):
+	userDestination = user.directions(user_posn, net_posns[closest])[routes]
+	toUser = user.directions(net_posns[closest], user_posn)[routes]
+	duration = userDestination[routes][legs][duration][value]
+
